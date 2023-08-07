ece4da0c9825cb4e189e04ad5b9c11a4cf36fef3
AmbikeshArman <ambikesharman2002@gmail.com>
2023-08-07 23:44:26 +0530

    Created a loader and reducer

--- frontend/src/Screens/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen.jsx
@@ -1,31 +1,32 @@
 import React from "react";
-import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../Components/Product";
-import axios from "axios";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
+import Loader from "../Components/Loader";
+import Message from "../Components/Message";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
-
+  const { data: products, isLoading, error } = useGetProductsQuery();
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>
+          {error?.data?.Message || error.error}
+        </Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
