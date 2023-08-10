c7468cf2dcf63299cc578fab9266c1133c06c489
AmbikeshArman <ambikesharman2002@gmail.com>
2023-08-10 15:34:41 +0530

    Backend authentication

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,11 +1,17 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
 
+//@desc   Fetch all Products
+//@route  Get /api/products
+//@access Public
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.json(products);
 });
 
+//@desch  Fetch a Product
+//@route  Get /api/products/:id
+//@access Public
 const getProductbyId = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (product) {
