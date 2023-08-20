07d4caf1707ef878d8c37cb82ee634471ea4ae14
AmbikeshArman <ambikesharman2002@gmail.com>
2023-08-20 20:11:23 +0530

    Intergrated PayPal

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -50,7 +50,7 @@ const getMyOrders = asyncHandler(async (req, res) => {
 
 //@desc   Get order by ID
 //@route  Get /api/orders/:id
-//@access Private/Admin
+//@access Private
 const getOrderById = asyncHandler(async (req, res) => {
   const order = await Order.findById(req.params.id).populate(
     "user",
@@ -65,14 +65,31 @@ const getOrderById = asyncHandler(async (req, res) => {
 });
 
 //@desc   Update order to paid
-//@route  Get /api/orders/:id/pay
+//@route  Put /api/orders/:id/pay
 //@access Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send("update order to paid");
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.staus,
+      update_time: req.body.update_time,
+      email_address: req.body.email_address,
+    };
+    const updatedOrder = await order.save();
+
+    res.status(200).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 //@desc   Update order to delivered
-//@route  Get /api/orders/:id/deliver
+//@route  Put /api/orders/:id/deliver
 //@access Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
   res.send("update order to delivered");
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import { Provider } from "react-redux";
 import store from "./store";
 // import 'bootstrap/dist/css/bootstrap.min.css';
@@ -22,6 +23,7 @@ import RegisterScreen from "./Screens/RegisterScreen";
 import ShippingScreen from "./Screens/ShippingScreen";
 import PaymentScreen from "./Screens/PaymentScreen";
 import PlaceOrderScreen from "./Screens/PlaceOrderScreen";
+import OrderScreen from "./Screens/OrderScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -35,6 +37,7 @@ const router = createBrowserRouter(
         <Route path='/shipping' element={<ShippingScreen />} />
         <Route path='/payment' element={<PaymentScreen />} />
         <Route path='/placeorder' element={<PlaceOrderScreen />} />
+        <Route path='/order/:id' element={<OrderScreen />} />
       </Route>
     </Route>
   )
@@ -44,7 +47,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
