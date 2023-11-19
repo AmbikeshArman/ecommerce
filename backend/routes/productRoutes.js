e1d6bc549cd9a0255416943f8691b0d0e4b273b0
AmbikeshArman <ambikesharman2002@gmail.com>
2023-11-19 14:39:22 +0530

    Completed Product func for admin

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,19 @@ import express from "express";
 import {
   getProducts,
   getProductbyId,
+  createProduct,
+  updateProduct,
+  deleteProduct,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").get(getProducts);
-router.route("/:id").get(getProductbyId);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router
+  .route("/:id")
+  .get(getProductbyId)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 export default router;
